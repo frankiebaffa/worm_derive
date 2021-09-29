3084a05ec92cac0c4540027608b896dbc86b1f58
Frankie Baffa <frankiebaffa@gmail.com>
2021-09-29 18:54:56 -0400

    continue developing procedural macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,16 @@ use {
         ToTokens,
     },
     proc_macro::TokenStream,
-    quote::quote,
+    quote::{
+        quote,
+        TokenStreamExt,
+    },
     syn::{
         DeriveInput,
         parse_macro_input,
+
     },
 };
-#[derive(Default, FromMeta)]
-struct DbModelTable {
-    db: String,
-    name: String,
-    alias: String,
-}
 #[derive(Default, Debug, FromMeta)]
 struct DbModelColumn {
     name: String,
@@ -29,6 +27,13 @@ struct DbModelColumnOpts {
     column: DbModelColumn,
     ident: Option<syn::Ident>,
 }
+#[derive(Default, FromMeta)]
+struct DbModelTable {
+    db: String,
+    name: String,
+    alias: String,
+    bool_flag: Option<String>,
+}
 #[derive(FromDeriveInput)]
 #[darling(attributes(dbmodel))]
 struct DbModelOpts {
@@ -37,7 +42,7 @@ struct DbModelOpts {
     data: ast::Data<(), DbModelColumnOpts>,
 }
 #[proc_macro_derive(Worm, attributes(dbmodel, dbcolumn))]
-pub fn derive_worm(input: TokenStream) -> TokenStream {
+pub fn derive_dbmodel(input: TokenStream) -> TokenStream {
     let d_input = parse_macro_input!(input as DeriveInput);
     let opts = DbModelOpts::from_derive_input(&d_input).unwrap();
     let db = opts.table.db.as_str();
@@ -46,22 +51,50 @@ pub fn derive_worm(input: TokenStream) -> TokenStream {
     let name = opts.ident;
     let mut columns = Vec::new();
     let mut idents = Vec::new();
+    let mut vars = Vec::new();
+    let mut bool_flag_ident = None;
+    let bool_flag = opts.table.bool_flag;
     let data = opts.data.take_struct().unwrap();
     for field in data {
         let ident = field.ident.unwrap();
         let column = field.column;
-        columns.push(column.name);
-        idents.push(ident);
+        let var = ident.to_token_stream();
+        columns.push(column.name.clone());
+        idents.push(ident.clone());
+        vars.push(var);
+        if !bool_flag.is_some() {
+            let bool_flag_res = bool_flag.clone().unwrap();
+            if bool_flag_res.eq(&column.name.clone()) {
+                bool_flag_ident = Some(ident);
+            }
+        }
     }
-    (quote! {
-        impl DbModel2 for #name {
+    let mut traits = quote! {};
+    let dbmodel_trait = quote! {
+        impl worm::traits::dbmodel::DbModel for #name {
             const DB: &'static str = #db;
             const TABLE: &'static str = #table;
             const ALIAS: &'static str = #alias;
-            fn from_row2() {
-                #(let #idents = #columns;)*
+            fn from_row(row: &rusqlite::Row) -> Result<#name, rusqlite::Error>{
+                use worm::traits::helpers::ColumnValue;
+                #(let #idents = row.value(&#columns)?;)*
+                return Ok(#name { #(#idents: #idents, )*});
             }
         }
-    }).into()
+    };
+    traits.append_all(dbmodel_trait);
+    if !bool_flag_ident.is_some() && !columns.contains(&name.to_string()) {
+        let bool_flag_ident_res = bool_flag_ident.unwrap();
+        let boolflag_trait = quote! {
+            //impl worm::traits::activeflag::ActiveFlag for #name {
+            //    const ACTIVE: &'static str = #bool_flag;
+            //    fn get_active(&self) -> bool {
+            //        return self.#bool_flag_ident_res;
+            //    }
+            //}
+        };
+        traits.append_all(boolflag_trait);
+    }
+    traits.into()
 }
 
